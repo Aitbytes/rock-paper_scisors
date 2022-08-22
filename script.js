dad525d1d958f106b455fdfc8eb74cba8d3661bd
Aitbytes <aittaleb.med08@gmail.com>
2022-08-22 21:08:38 +0100

    Define getComputerChoice
    Define getComputerChoice to randomly return one of the following three:
    rock, paper, or scisors.

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 
 function getComputerChoice() {
+    const num = Math.floor(Math.random()*3) + 1;
+
+    if (num === 1) {
+        return "rock";
+    }else if (num === 2) {
+        return "paper";
+    }else {
+        return "scisors";
+    }
 
 
 };
